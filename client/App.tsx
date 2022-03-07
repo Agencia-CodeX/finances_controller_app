8ae32c231dfb3b72d68b2e6e06c480eed0acca9a
Veronezegui <veronezegui@gmail.com>
2022-03-07 15:10:02 -0300

    Primeiras configs

--- client/App.tsx
+++ client/App.tsx
@@ -1,19 +1,32 @@
+import {
+  useFonts,
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_700Bold,
+} from "@expo-google-fonts/poppins";
+import AppLoading from "expo-app-loading";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { View, Text } from "react-native";
+import { ThemeProvider } from "styled-components";
+
+import theme from "./src/global/styles/theme";
 
 export default function App() {
+  const [fonstLoaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_700Bold,
+  });
+
+  if (!fonstLoaded) {
+    return <AppLoading />;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>App is runnig!</Text>
-    </View>
+    <ThemeProvider theme={theme}>
+      <View>
+        <Text>Hello World</Text>
+      </View>
+    </ThemeProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
---
+++ client/src/global/styles/theme.ts
@@ -0,0 +1,16 @@
+export default {
+  colors: {
+    background: "#0D0E1E",
+
+    gradient1: "#5F7EFF",
+    gradient2: "#9245DE",
+
+    fontsColor: "#FFFFFF",
+  },
+
+  fonts: {
+    regular: "Poppins_400Regular",
+    medium: "Poppins_500Medium",
+    bold: "Poppins_700Bold",
+  },
+};
