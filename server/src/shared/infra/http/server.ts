b80c8474bb38c984136475d9e96486288edc033d
MiguelMarcola <miguelmarcola@gmail.com>
2022-03-23 17:22:41 -0300

    refactoring

--- server/src/shared/infra/http/server.ts
+++ server/src/shared/infra/http/server.ts
@@ -1,7 +1,7 @@
 import "reflect-metadata";
 import express from "express";
 
-import "../../../database/infra/typeorm";
+import "./typeorm";
 import { router } from "./routes";
 
 const app = express();
