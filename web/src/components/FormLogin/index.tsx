fbfda7cd7c4880b3dfae40dd4a6e1e92fbe81e6c
MiguelMarcola <miguelmarcola@gmail.com>
2022-04-04 23:49:38 -0300

    validate login form fields

--- web/src/components/FormLogin/index.tsx
+++ web/src/components/FormLogin/index.tsx
@@ -1,18 +1,71 @@
+import { FormEvent, useState } from "react";
+import { toast } from "react-toastify";
+
+import { validateEmail } from "../../utils/ValidateEmails";
+import { ValidatePassaword } from "../../utils/ValidatePassword";
 import styles from "./styles.module.scss";
 
 export function FormLogin() {
+    const [email, setEmail] = useState("");
+    const [password, setPassaword] = useState("");
+
+    async function handleLogin(event: FormEvent) {
+        event.preventDefault();
+
+        if (!email) {
+            toast.error("Digite seu E-mail!");
+            return;
+        }
+
+        if (!validateEmail(email)) {
+            toast.error("E-mail inválido!");
+            return;
+        }
+
+        if (!password) {
+            toast.error("Digite sua senha");
+            return;
+        }
+
+        const ValidateStrongPassaword = ValidatePassaword(password);
+        console.log(ValidateStrongPassaword);
+
+        if (!ValidateStrongPassaword.result) {
+            ValidateStrongPassaword.message.map((message) =>
+                toast.error(message)
+            );
+            return;
+        }
+
+        console.log(email, password);
+
+        setEmail("");
+        setPassaword("");
+    }
+
     return (
         <section className={styles.contentForm}>
-            <div className={styles.formBox}>
-                <h4>Login</h4>
-                <form>
-                    <input type="text" placeholder="E-mail" />
-                    <input type="password" placeholder="Senha" />
-                </form>
-            </div>
-            <button className={styles.loginButton} type="button">
-                Entrar
-            </button>
+            <form onSubmit={handleLogin}>
+                <div className={styles.formBox}>
+                    <h4>Login</h4>
+                    <input
+                        type="text"
+                        placeholder="E-mail"
+                        value={email}
+                        onChange={(event) => setEmail(event.target.value)}
+                    />
+                    <input
+                        type="password"
+                        placeholder="Senha"
+                        value={password}
+                        onChange={(event) => setPassaword(event.target.value)}
+                    />
+                </div>
+                <button className={styles.loginButton} type="submit">
+                    Entrar
+                </button>
+            </form>
+
             <a href="/register">
                 <button className={styles.registerButton} type="button">
                     Cadastrar-se
